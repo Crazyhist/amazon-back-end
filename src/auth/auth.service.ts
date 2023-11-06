941de615c252af945ea1c7943043b5f4f800fd2e
Boss <crazyhistorian2000@yandex.ru>
2023-11-07 02:37:38 +0300

    add config and JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { AuthDto } from './auth.dto';
+import { BaseExceptionFilterContext } from '@nestjs/core/exceptions/base-exception-filter-context';
+import { faker } from '@faker-js/faker';
+import { hash } from 'argon2';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {
+    constructor(private prisma: PrismaService) {}
+    async register(dto: AuthDto) {
+       const oldUser = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email
+            }
+        })
+    
+        if (oldUser) throw new BadRequestException('User already exists')
+
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                name: faker.name.firstName(),
+                avatarPath: faker.image.avatar(),
+                phone: faker.phone.number('+7 (###) ###-##-##'),
+                password: await hash(dto.password)
+            }
+        })
+        return user
+    }
+    private async issueToken(userId: number){
+        const data = {id: userId}
         
-     async register(dto: AuthDto) {
-    //     const oldUser = await this.prisma.user.findUnique({
-    //         where: {
-    //             email: dto.email
-    //         }
-    //     })
-    // }
+        // const accessToken = this.jwtService.sign(data, {})
     }
 }
