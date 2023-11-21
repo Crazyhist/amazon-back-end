70e7b6222e0a57e83b04f67a906c8340978bfc61
Boss <crazyhistorian2000@yandex.ru>
2023-11-21 18:01:58 +0300

    add auth suka

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,11 +4,13 @@ import { AuthDto } from './auth.dto';
 import { faker } from '@faker-js/faker';
 import { hash } from 'argon2';
 import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService, private jwt: JwtService) {}
 
+    //Получение старого юзера
     async register(dto: AuthDto) {
        const oldUser = await this.prisma.user.findUnique({
             where: {
@@ -18,20 +20,42 @@ export class AuthService {
     
         if (oldUser) throw new BadRequestException('User already exists')
 
+        // Создание бзера (фейкового)
         const user = await this.prisma.user.create({
             data: {
                 email: dto.email,
                 name: faker.name.firstName(),
                 avatarPath: faker.image.avatar(),
                 phone: faker.phone.number('+7 (###) ###-##-##'),
-                password: await hash(dto.password)
+                password: await hash(dto.password) // Хешурем пароль в не понятном формате 
             }
         })
-        return user
+
+        const tokens = await this.issueToken(user.id)
+
+        return {
+            user: this.returnUserFields(user),
+            ...tokens,
+        }
     }
     private async issueToken(userId: number){
-        const data = {id: userId}
+        const data = {id: userId} // payload записываем внутри токена
         
-        // const accessToken = this.jwtService.sign(data, {})
+        const accessToken = this.jwt.sign(data, {
+            expiresIn: '1h',
+        })
+
+        const refreshToken = this.jwt.sign(data, {
+            expiresIn: '7d',
+        })
+
+        return {accessToken, refreshToken}
+    }
+
+    private returnUserFields(user: User) {
+        return {
+            id: user.id,
+            email: user.email,
+        }
     }
 }
