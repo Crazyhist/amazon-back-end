ac9d6825814436d25e4ecd8e34cf8e3c2fa6debd
Boss <crazyhistorian2000@yandex.ru>
2023-11-23 15:47:21 +0300

    add dto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,24 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
-import { AuthDto } from './auth.dto';
-import { faker } from '@faker-js/faker';
+import { AuthDto } from './dto/auth.dto';
+import { faker, th } from '@faker-js/faker';
 import { hash } from 'argon2';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '@prisma/client';
+import { error } from 'console';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService, private jwt: JwtService) {}
 
+    async getNewTokens(refreshToken: string){
+        const result = await this.jwt.verifyAsync(refreshToken) // расскрываем, забираем id
+        if(result) throw new UnauthorizedException('invalid refresh token')
+
+        const user = await this.prisma.user.findUnique({where: {
+            id: result.id,
+        }})
+    }
     //Получение старого юзера
     async register(dto: AuthDto) {
        const oldUser = await this.prisma.user.findUnique({
@@ -20,7 +29,7 @@ export class AuthService {
     
         if (oldUser) throw new BadRequestException('User already exists')
 
-        // Создание бзера (фейкового)
+        // Создание юзера (фейкового)
         const user = await this.prisma.user.create({
             data: {
                 email: dto.email,
@@ -35,7 +44,7 @@ export class AuthService {
 
         return {
             user: this.returnUserFields(user),
-            ...tokens,
+            ...tokens
         }
     }
     private async issueToken(userId: number){
